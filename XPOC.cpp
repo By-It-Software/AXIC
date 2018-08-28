5f6624dd4873e67fd5ad683bc6b29d3fc61288b6
By-It-Software <33151736+By-It-Software@users.noreply.github.com>
2018-08-28 05:32:47 +0200

    beta file (for dev only)

---
+++ XPOC.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+
+
+int next_button;
+int prev_button;
+int address_button;
+int del_button;
+int memlocate;
+int memrecognize;
+int sudo_apt_get_refresh;
+int locate_address;
+int display_AXIC;
+int AXIC_MAIN;
+int function:CALL(HYPER-V-virtual);
+FUNCTION:CALL(GO);
+{
+    //This source works only on AXIC Machine
+    /AXIC_MAIN.GO
+        main.display(rel&display_AXIC);
+        main.report(bug.reporter);
+            function:CALL(remove_extent=="Kext_address");
+            function:CALL(locate_address);
+                if (locate_address=memlocate,memrecognize){
+                        function:CALL(address++address_recognize);
+                        cout<<"Kernel Address Injected On"<<address<<endl;
+                        cout<<endl;
+                }
+                else if (locate_address != *){
+                    delete dir(template\res\go\goforce.cpp);
+                }
+                AXIC_MAIN.GO/
+    }
+
+
+//SIGN.GO
+
+
+
+string a_button ="A";
+string b_button ="B";
+string c_button ="C";
+string d_button ="D";
+string e_button ="E";
+string f_button ="F";
+string g_button ="G";
+
+int button_0 = 0;
+int button_1 = 1;
+int button_2 = 2;
+int button_3 = 3;
+int button_4 = 4;
+int button_5 = 5;
+
+int address1;
+int address2;
+int address3;
+int address4;
+int address5;
+int address6;
+int address7;
+}
---
+++ res/go-axic.go
@@ -0,0 +1,34 @@
+//Personal GO script.
+
+//AXIC BY IT SOFTWARE BETA 0.000001
+
+
+//Main
+
+#include ../rby/../.../axic.h
+
+int main{
+	
+		axic.go(start){
+			SYSTEM.CALL:FUNCTION(AXIC.OS);
+			SYSTEM.CALL:FUNCTION(AXIC.CPP);
+			SYSTEM.CALL:FUNCTION(../XPOC.CPP);
+			SYSTEM.CALL:FUNCTION(../Z80.AXIC);
+			SYSTEM.CALL:FUNCTION(../lib/AXIC.DLL);
+			Console.output(*);
+			
+				//MEMORY LOCATION
+			go.createpath(go){
+				go.createpath:("temp/&storage;%mp,e,f,g,a,f");
+				go.createlocation(2000); //mb valor
+				go.createaddress;
+				go.createaddressmem;
+				go.creatememory;
+				go.&misc;(mem.create_address);
+				go.output = memoryaddress;
+				output.console;
+				define console = char(utf-8);
+				//To use this create your personal script
+			}
+		}
+}
